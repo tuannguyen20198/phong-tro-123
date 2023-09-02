cd8a9a99407d75d51e1625cea550288981f9da9c
tuannguyen20198 <lntt20198@gmail.com>
2023-09-02 20:08:49 +0700

    config app connect server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
 import express from "express";
+require("dotenv").config();
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res) => {
+  res.send("server on ...");
+});
+
+const port = process.env.PORT || 8888;
+const listener = app.listen(port, () => {
+  console.log(`Server is running on the port ${listener.address().port}`);
+});
