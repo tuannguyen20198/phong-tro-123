4fcb8401bc94f4b7775411f1575c86368a73fcfa
tuannguyen20198 <lntt20198@gmail.com>
2023-09-02 19:37:55 +0700

    code Home

--- client/src/containers/Public/Home.js
+++ client/src/containers/Public/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="w-full flex flex-col items-center h-full border border-red-500">
       <Header />
       <Navigation />
-      <div className="w-1100 flex flex-col items-center justify-start">
+      <div className="w-1100 flex flex-col items-center mt-3 justify-start">
         <Outlet />
       </div>
     </div>
