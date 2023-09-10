d67629ab7c13cb5bdf8bf5c58a023fae4b5d451d
tuannguyen20198 <lntt20198@gmail.com>
2023-09-10 16:38:01 +0700

    fix prettierrc

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,12 +1,11 @@
-import React, { memo } from "react";
+import React, { memo } from 'react';
 const Button = ({ text, textColor, bgColor, IcAfter, onClick, fullWidth }) => {
   return (
     <div>
       <button
         type="button"
-        className={`p-2 ${textColor} ${bgColor} outline-none rounded-md hover:underline flex items-center justify-center gap-1 ${
-          fullWidth && "w-full"
-        }`}
+        className={`p-2 ${textColor} ${bgColor} outline-none rounded-md hover:underline flex items-center justify-center gap-1 ${fullWidth &&
+          'w-full'}`}
         onClick={onClick}
       >
         <span>{text}</span>
