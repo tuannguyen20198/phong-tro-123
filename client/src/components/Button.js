2aed513777d8e37561f2285121dcd7ba56b108b5
tuannguyen20198 <lntt20198@gmail.com>
2023-09-11 19:12:19 +0700

    fix bug

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,11 +1,11 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 const Button = ({ text, textColor, bgColor, IcAfter, onClick, fullWidth }) => {
   return (
     <div>
       <button
         type="button"
         className={`p-2 ${textColor} ${bgColor} outline-none rounded-md hover:underline flex items-center justify-center gap-1 ${fullWidth &&
-          'w-full'}`}
+          "w-full"}`}
         onClick={onClick}
       >
         <span>{text}</span>
