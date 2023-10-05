615eceaf8bb058a516da2fc5aa579fe5bf72f65e
tuannguyen20198 <lntt20198@gmail.com>
2023-10-05 22:25:51 +0700

    code address and provinces page Admin

--- client/src/components/Address.js
+++ client/src/components/Address.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import {SelectAddress} from "../components"
+import {apiGetPublicDistrict, apiGetPublicProvinces } from '../services' 
 
 const Address = () => {
+  const [provinces, setProvinces] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  
+  const [province, setProvince] = useState();
+  const [district, setDistrict] = useState();
+  const [reset, setReset] = useState(false);
+  
+  
+
+  useEffect(() => {
+    const fetchPublicProvince = async() => {
+      const response = await apiGetPublicProvinces()
+      if (response.status === 200) {
+        setProvinces(response?.data?.results)
+      }
+    }
+    fetchPublicProvince()
+  }, [])
+  useEffect(() => {
+    setDistrict(null)
+    const fetchPublicDistrict = async() => {
+      const response = await apiGetPublicDistrict(province)
+      if (response.status === 200) {
+        setDistricts(response?.data?.results)
+      }
+      console.log(response)
+    }
+    province && fetchPublicDistrict()
+    !province ? setReset(true) : setReset(false)
+    !province && setDistricts([])
+  },[province])
   return (
     <div>
       <h2 className='font-semibold text-xl py-4'>Địa chỉ cho thuê</h2>
       <div className='flex flex-col gap-4'>
         <div className='flex items-center gap-4'>
-          <SelectAddress label='Tỉnh/Thành phố'/>
-          <SelectAddress label='Quận/Huyện'/>
+          <SelectAddress type="province"  value={province} setValue={setProvince} options={provinces} label='Tỉnh/Thành phố'/>
+          <SelectAddress reset={reset} type="district"  value={district} setValue={setDistrict} options={districts} label='Quận/Huyện'/>
         </div>
-        <input value="123" type="text" readOnly className='border border-gray-200 rounded-md bg-gray-100 p-2 w-full outline-none'/>
+        <div className='flex flex-col gap-2'>
+          <label className='font-medium' htmlFor="exactly-address">Địa chỉ chính xác </label>
+          <input 
+            type="text" 
+            id='exactly-address' 
+            readOnly 
+            className='border border-gray-200 rounded-md bg-gray-100 p-2 w-full outline-none'
+            value={`${district ? `${districts?.find(item => item.district_id === district)?.district_name},` : ''} ${province ? `${provinces?.find(item => item.province_id === province)?.province_name}` : ''}`}
+          />
+        </div>
       </div>
     </div>
   )
--- client/src/components/SelectAddress.js
+++ client/src/components/SelectAddress.js
@@ -1,11 +1,24 @@
 import React, { memo } from 'react'
 
-const SelectAddress = ({label}) => {
+const SelectAddress = ({ label , options ,value , setValue , type , reset }) => {
+  
   return (
     <div className='flex flex-col gap-2 w-full flex-1'>
         <label className='font-medium' htmlFor="select-address">{label}</label>
-        <select id="select-address" className='outline-none border border-gray-300 px-1 w-full p-2 rounded-md'>
+        <select value={reset ? '' : value} onChange={(e) => setValue(e.target.value)} id="select-address" className='outline-none border border-gray-300 px-1 w-full p-2 rounded-md'>
             <option value="">{`-- Chọn ${label}--`}</option>
+            {options?.map(item => {
+                return (
+                    <option 
+                        key={type === 'province'  ? item?.province_id : item?.district_id} 
+                        value={type === 'province'  
+                        ? item?.province_id
+                        : item?.district_id}
+                        >
+                            {type === "province" ? item?.province_name: item?.district_name}
+                    </option>
+                )
+            })}
         </select>
     </div>
   )
